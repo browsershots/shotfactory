38c58c70cdd24ea539a1a47e66fd118724ac1e65
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-06-09 11:08:15 +0000

    Make PNG writer accept raw pixel data (without scanline filtering indicators). Moved interlacing code into write(). Using zlib compressor object.
    git-svn-id: http://browsershots.googlecode.com/svn/trunk/shotfactory@329 1175509c-980b-11dd-9476-71c886a649bc

--- lib/image/png.py
+++ lib/image/png.py
@@ -28,6 +28,15 @@ __author__ = '$Author$'
 
 import sys, zlib, struct
 
+# http://www.w3.org/TR/PNG/#8InterlaceMethods
+adam7 = [(0, 0, 8, 8),
+         (4, 0, 8, 8),
+         (0, 4, 4, 8),
+         (2, 0, 4, 4),
+         (0, 2, 2, 4),
+         (1, 0, 2, 2),
+         (0, 1, 1, 2)]
+
 def write_chunk(outfile, tag, data):
     """
     Write a PNG chunk to the output file, including length and checksum.
@@ -40,45 +49,44 @@ def write_chunk(outfile, tag, data):
     checksum = zlib.crc32(data, checksum)
     outfile.write(struct.pack("!I", checksum))
 
-def write(outfile, width, height, pixels, interlaced = False):
+def write(outfile, width, height, pixels, interlace = False):
     """
     Write a 8bpp RGB opaque PNG file to the output file.
     http://www.w3.org/TR/PNG/
     """
     # http://www.w3.org/TR/PNG/#5PNG-file-signature
     outfile.write(struct.pack("8B", 137, 80, 78, 71, 13, 10, 26, 10))
-    # http://www.w3.org/TR/PNG/#11IHDR
-    if interlaced:
-        write_chunk(outfile, 'IHDR', struct.pack("!2I5B", width, height, 8, 2, 0, 0, 1))
+
+    if interlace:
+        interlace = 1
     else:
-        write_chunk(outfile, 'IHDR', struct.pack("!2I5B", width, height, 8, 2, 0, 0, 0))
+        interlace = 0
+
+    # http://www.w3.org/TR/PNG/#11IHDR
+    write_chunk(outfile, 'IHDR', struct.pack("!2I5B", width, height, 8, 2, 0, 0, interlace))
+
+    compressor = zlib.compressobj(9)
+    compressed = []
+    scanline = 3*width
+    if not interlace:
+        for y in range(height):
+            compressed.append(compressor.compress(chr(0)))
+            offset = y*scanline
+            compressed.append(compressor.compress(pixels[offset:offset+scanline]))
+    else:
+        for xstart, ystart, xstep, ystep in adam7:
+            for y in range(ystart, height, ystep):
+                if xstart < width:
+                    compressed.append(compressor.compress(chr(0)))
+                for x in range(xstart, width, xstep):
+                    offset = scanline*y + 3*x
+                    compressed.append(compressor.compress(pixels[offset:offset+3]))
+    compressed.append(compressor.flush())
+
     # http://www.w3.org/TR/PNG/#11IDAT
-    write_chunk(outfile, 'IDAT', zlib.compress(pixels, 9))
+    write_chunk(outfile, 'IDAT', ''.join(compressed))
     # http://www.w3.org/TR/PNG/#11IEND
     write_chunk(outfile, 'IEND', '')
 
-def interlace(width, height, pixels):
-    """
-    Interlace pixel data into 7 consecutive passes.
-    http://www.w3.org/TR/PNG/#8Interlace
-    """
-    result = []
-    passes = [(0, 0, 8, 8),
-              (4, 0, 8, 8),
-              (0, 4, 4, 8),
-              (2, 0, 4, 4),
-              (0, 2, 2, 4),
-              (1, 0, 2, 2),
-              (0, 1, 1, 2)]
-    scanline = 1+3*width
-    for xstart, ystart, xstep, ystep in passes:
-        for y in range(ystart, height, ystep):
-            if xstart < width:
-                result.append(chr(0))
-            for x in range(xstart, width, xstep):
-                offset = 1 + scanline*y + 3*x
-                result.append(pixels[offset:offset+3])
-    return ''.join(result)
-
 if __name__ == '__main__':
-    write(sys.stdout, 1, 1, interlace(1, 1, chr(0) + 'abc'))
+    write(sys.stdout, 2, 2, '~##' + '#~#' + '~~#' + '##~', interlace = True)
