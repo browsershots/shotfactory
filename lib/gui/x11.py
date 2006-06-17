4d69befb9914b4cf6b2e6318217b661e56b4ac3d
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-06-17 18:10:13 +0000

    Rearranging code with object-oriented approach to portability across different GUIs.
    git-svn-id: http://browsershots.googlecode.com/svn/trunk/shotfactory@514 1175509c-980b-11dd-9476-71c886a649bc

--- lib/gui/x11.py
+++ lib/gui/x11.py
@@ -24,18 +24,28 @@ __revision__ = '$Rev$'
 __date__ = '$Date$'
 __author__ = '$Author$'
 
-import os, time
-from array import array
+
+import os
 from shotfactory03.gui.base import BaseGui
-from shotfactory03.image import hashmatch
-from shotfactory03.pypng import png
+
 
 class X11Gui(BaseGui):
     """
     Special functions for the X11 screen.
     """
 
-    def __init__(self, display=0):
+    def __init__(self, width, height, bpp, dpi, display=1):
+        """
+        Start a VNC server.
+        """
+        command = ('vncserver :%d -geometry %dx%d -depth %d -dpi %d'
+                   % (display, width, height, bpp, dpi))
+        error = os.system(command)
+        assert not error
+        self.width = width
+        self.height = height
+        self.bpp = bpp
+        self.dpi = dpi
         self.display = display
 
     def shell(self, command):
@@ -85,95 +95,3 @@ class X11Gui(BaseGui):
     def screenshot(self, filename):
         """Save the full screen to a PPM file."""
         self.shell('xwd -root -silent | xwdtopnm | pnmdepth 255 > "%s"' % filename)
-
-    def page_filename(self, page_number, direction='dn'):
-        """Create a PPM filename."""
-        return 'pg%s%02d.ppm' % (direction, page_number)
-
-    def scroll_pages(self, good_offset=300):
-        """
-        Take screenshots and scroll down between them.
-        """
-        filename = self.page_filename(1)
-        pixels_per_line = 50
-        scroll_lines = good_offset / pixels_per_line
-        offsets = []
-        for page in range(2, 20):
-            for dummy in range(scroll_lines):
-                self.down()
-            time.sleep(0.1)
-
-            previous = filename
-            filename = self.page_filename(page)
-            self.screenshot(filename)
-            offset = hashmatch.find_offset(previous, filename)
-
-            if not offset:
-                break
-            offsets.append(offset)
-
-            apparently = offset / scroll_lines
-            if apparently == 0:
-                print ("apparently no offset per keypress: %d/%d=%d"
-                       %(offset, scroll_lines, apparently))
-            elif apparently != pixels_per_line:
-                pixels_per_line = apparently
-                scroll_lines = min(good_offset / pixels_per_line, 40)
-                print ("%d pixels/keypress, %d keypresses/scroll"
-                       % (pixels_per_line, scroll_lines))
-        return offsets
-
-    def scanlines(self, width, height, offsets):
-        """
-        Merge multi-page screenshots and yield scanlines.
-        """
-        overlaps = []
-        for offset in offsets:
-            overlaps.append(height - offset)
-        print 'offsets: ', offsets
-        print 'overlaps:', overlaps
-        total = 0
-        row_bytes = 3*width
-        for index in range(0, len(overlaps) + 1):
-            top = 0
-            bottom = 0
-            if index > 0:
-                top = overlaps[index-1] / 2
-            if index < len(offsets):
-                bottom = (overlaps[index]+1) / 2
-            segment = height - top - bottom
-            total += segment
-            bottom = height - bottom
-            filename = self.page_filename(index+1)
-            print filename, top, bottom, segment, total
-            infile = open(filename, 'rb')
-            hashmatch.read_ppm_header(infile)
-            infile.seek(top*row_bytes, 1)
-            for y in range(top, bottom):
-                scanline = array('B')
-                scanline.fromfile(infile, row_bytes)
-                yield scanline
-            infile.close()
-
-
-    def browsershot(self, pngfilename = 'browsershot.png'):
-        """
-        Take a number of screenshots and merge them into one tall image.
-        """
-        self.hide_mouse()
-        time.sleep(0.1)
-
-        filename = self.page_filename(1)
-        self.screenshot(filename)
-        magic, width, height, maxval = hashmatch.read_ppm_header(open(filename, 'rb'))
-        assert magic == 'P6'
-        assert maxval == 255
-
-        offsets = self.scroll_pages(good_offset=height/2)
-        total = height + sum(offsets)
-        print 'total:', total
-        scanlines = self.scanlines(width, height, offsets)
-
-        outfile = file(pngfilename, 'wb')
-        png.write(outfile, scanlines, width, total)
-        outfile.close()
