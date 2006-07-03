4f2959468b8da2dd7d3c3915549f6f0f3ee698e0
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-07-03 16:44:15 +0000

    Fixed constant name.
    git-svn-id: http://browsershots.googlecode.com/svn/trunk/shotfactory@588 1175509c-980b-11dd-9476-71c886a649bc

--- lib/gui/windows.py
+++ lib/gui/windows.py
@@ -59,7 +59,8 @@ class WindowsGui(BaseGui):
     def scroll_down(self, pixels):
         """Scroll down a number of pixels."""
         win32gui.PostMessage(self.scroll_window,
-                             win32con.WM_VSCROLL, win32con.SB_DOWN, 0)
+                             win32con.WM_VSCROLL,
+                             win32con.SB_LINEDOWN, 0)
 
     def start_browser(self, browser, url):
         """Start browser and load website."""
