5502c15d1340aeeb418001b087d5f19e69aac9fd
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2006-09-08 16:01:27 +0000

    Recover from crashed VNC server.
    git-svn-id: http://browsershots.googlecode.com/svn/trunk/shotfactory@751 1175509c-980b-11dd-9476-71c886a649bc

--- lib/gui/x11.py
+++ lib/gui/x11.py
@@ -42,13 +42,16 @@ class X11Gui(BaseGui):
         self.display = display
         command = ('vncserver %s -geometry %dx%d -depth %d -dpi %d'
                    % (display, width, height, bpp, dpi))
-        for attempts in range(10):
+        for attempts in range(1, 11):
             error = os.system(command)
             if not error:
                 break
-            print 'could not start vncserver, trying again in 3 seconds'
+            print 'vncserver error (attempt %d out of 10)' % attempts
             time.sleep(3)
         if error:
+            os.system('vncserver -kill %s' % display)
+            time.sleep(10)
+            os.system('killall -9 vncserver')
             raise RuntimeError('could not start vncserver')
 
     def shell(self, command):
