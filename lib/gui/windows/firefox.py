b39f926d2ec56740c217f456ce3bcae526f55b7a
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-02-03 20:53:59 +0000

    Fixed semantic error: Gui.down() is supposed to scroll one line only.
    git-svn-id: http://browsershots.googlecode.com/svn/trunk/shotfactory@969 1175509c-980b-11dd-9476-71c886a649bc

--- lib/gui/windows/firefox.py
+++ lib/gui/windows/firefox.py
@@ -35,12 +35,10 @@ class Gui(windows.Gui):
     """
 
     def down(self):
-        """Scroll down half a screen."""
+        """Scroll down one line."""
         firefox = self.find_window_by_title_suffix('Firefox')
         scrollable = win32gui.GetWindow(firefox, win32con.GW_CHILD)
-        for count in range(10):
-            self.send_keypress(scrollable, win32con.VK_DOWN)
-            time.sleep(0.1)
+        self.send_keypress(scrollable, win32con.VK_DOWN)
 
     def remove_crash_dialog(self, browser):
         """Delete local application data for Firefox."""
