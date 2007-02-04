19a7a41e40a2f2087354db7366b258f0b5f777e8
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-02-04 07:48:57 +0000

    Purge Firefox cache before each screenshot.
    git-svn-id: http://browsershots.googlecode.com/svn/trunk/shotfactory@976 1175509c-980b-11dd-9476-71c886a649bc

--- lib/gui/windows/firefox.py
+++ lib/gui/windows/firefox.py
@@ -24,7 +24,7 @@ __revision__ = '$Rev: 503 $'
 __date__ = '$Date: 2006-06-17 08:14:59 +0200 (Sat, 17 Jun 2006) $'
 __author__ = '$Author: johann $'
 
-import os, time, sys
+import os, time, sys, shutil
 import win32api, win32gui, win32con, pywintypes
 from win32com.shell import shellcon, shell
 from shotfactory03.gui import windows
@@ -34,27 +34,33 @@ class Gui(windows.Gui):
     Special functions for Firefox on Windows.
     """
 
-    def down(self, verbose=False):
-        """Scroll down one line."""
-        firefox = self.find_window_by_title_suffix(' Firefox', verbose)
-        scrollable = self.get_child_window(firefox, verbose)
-        self.send_keypress(scrollable, win32con.VK_DOWN)
-        time.sleep(0.1)
-
-    def remove_crash_dialog(self, browser):
+    def remove_crash_dialog(self, verbose=True):
         """Delete local application data for Firefox."""
         appdata = shell.SHGetFolderPath(0, shellcon.CSIDL_APPDATA, 0, 0)
         profiles = os.path.join(appdata, 'Mozilla', 'Firefox', 'Profiles')
         for profile in os.listdir(profiles):
             session = os.path.join(profiles, profile, 'sessionstore.js')
             if os.path.exists(session):
-                print "deleting previous session", session
+                if verbose:
+                    print "deleting previous session:", session
                 os.unlink(session)
+
+    def empty_cache(self, verbose=True):
+        """Delete all files from the browser cache."""
+        appdata = shell.SHGetFolderPath(0, shellcon.CSIDL_LOCAL_APPDATA, 0, 0)
+        profiles = os.path.join(appdata, 'Mozilla', 'Firefox', 'Profiles')
+        for profile in os.listdir(profiles):
+            cache =  os.path.join(profiles, profile, 'Cache')
+            if os.path.exists(cache):
+                if verbose:
+                    print "deleting browser cache:", cache
+                shutil.rmtree(cache)
 
     def start_browser(self, config, url, options):
         """Start browser and load website."""
         self.close()
-        self.remove_crash_dialog(config['browser'])
+        self.remove_crash_dialog()
+        self.empty_cache()
         if config['command'] == 'firefox':
             command = r'c:\progra~1\mozill~1\firefox.exe'
         else:
@@ -63,6 +69,13 @@ class Gui(windows.Gui):
         os.spawnl(os.P_DETACH, command, os.path.basename(command), url)
         print "Sleeping %d seconds while page is loading." % options.wait
         time.sleep(options.wait)
+
+    def down(self, verbose=False):
+        """Scroll down one line."""
+        firefox = self.find_window_by_title_suffix(' Firefox', verbose)
+        scrollable = self.get_child_window(firefox, verbose)
+        self.send_keypress(scrollable, win32con.VK_DOWN)
+        time.sleep(0.1)
 
 
 if __name__ == '__main__':
