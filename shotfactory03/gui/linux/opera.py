fe23769af1486586e9778807c6c15a70004461b9
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-05-26 09:29:35 +0000

    Try to remove upgrade dialog.
    git-svn-id: http://browsershots.googlecode.com/svn/trunk/shotfactory@1315 1175509c-980b-11dd-9476-71c886a649bc

--- shotfactory03/gui/linux/opera.py
+++ shotfactory03/gui/linux/opera.py
@@ -18,6 +18,8 @@
 
 """
 GUI-specific interface functions for X11.
+
+>>> ini_find_section(TEST_LINES, 'User Prefs')
 """
 
 __revision__ = '$Rev$'
@@ -43,4 +45,50 @@ class Gui(base.Gui):
         inifile = home + '/.opera/opera6.ini'
         if os.path.exists(inifile):
             print 'removing crash dialog from', inifile
-            os.system("sed -i -e 's/^Run=[0-9]$/Run=0/g' " + inifile)
+            lines = file(inifile).readlines()
+            ini_set(lines, 'User Prefs', 'Run', 0)
+            ini_set(lines, 'User Prefs', 'Show New Opera Dialog', 1)
+            open(inifile, 'w').write(''.join(lines))
+
+def ini_set(lines, section, key, value):
+    if lines[0][-2] == chr(13):
+        crlf = lines[0][-2:]
+    else:
+        crlf = lines[0][-1]
+    start, stop = self.ini_find_section(lines, section)
+    if start is None:
+        lines.append(crlf)
+        lines.append('[' + section + ']')
+        lines.append(key + '=' + str(value) + crlf)
+    else:
+        index = self.ini_find_key_line(lines, start, stop, key)
+        if index:
+            lines[index] = key + '=' + str(value) + crlf
+        else:
+            lines.insert(stop, key + '=' + str(value) + crlf)
+
+
+def ini_find_section(lines, section):
+    start = None
+    for index, line in enumerate(lines):
+        if line.startswith('[' + section + ']'):
+            start = index
+        if start and line.strip() == '':
+            return start, index
+    if start:
+        return start, len(lines)
+    return None, None
+
+
+def ini_find_key_line(lines, start, stop, key):
+    for index in range(start, stop):
+        if lines[index].startswith(key + '='):
+            return index
+
+
+if __name__ == '__main__':
+    import doctest
+    TEST_LINES = [
+        "[User Prefs]",
+        ]
+    doctest.testmod()
