258aebd99e37bb3554fa18cf0e7d2956069249ec
johann <johann@1175509c-980b-11dd-9476-71c886a649bc>
2007-10-22 18:47:26 +0000

    Inherit from Firefox.
    git-svn-id: http://browsershots.googlecode.com/svn/trunk/shotfactory@2213 1175509c-980b-11dd-9476-71c886a649bc

--- shotfactory04/gui/linux/bonecho.py
+++ shotfactory04/gui/linux/bonecho.py
@@ -25,30 +25,11 @@ __author__ = "$Author$"
 
 import os
 import shutil
-from shotfactory04.gui import linux as base
+from shotfactory04.gui.linux import firefox as base
 
 
 class Gui(base.Gui):
     """
     Special functions for Mozilla Bon Echo.
     """
-
-    def reset_browser(self):
-        """
-        Delete crash dialog and browser cache.
-        """
-        home = os.environ['HOME'].rstrip('/')
-        dotdir = os.path.join(home, '.mozilla/firefox')
-        if not os.path.exists(dotdir):
-            return
-        for profile in os.listdir(dotdir):
-            # Delete cache
-            cachedir = os.path.join(dotdir, profile, 'Cache')
-            if os.path.exists(cachedir):
-                print 'deleting cache', cachedir
-                shutil.rmtree(cachedir)
-            # Delete crash dialog
-            crashfile = os.path.join(dotdir, profile, 'sessionstore.js')
-            if os.path.exists(crashfile):
-                print 'deleting crash file', crashfile
-                os.unlink(crashfile)
+    pass
--- shotfactory04/gui/linux/deerpark.py
+++ shotfactory04/gui/linux/deerpark.py
@@ -25,25 +25,11 @@ __author__ = "$Author: johann $"
 
 import os
 import shutil
-from shotfactory04.gui import linux as base
+from shotfactory04.gui.linux import firefox as base
 
 
 class Gui(base.Gui):
     """
     Special functions for Mozilla DeerPark.
     """
-
-    def reset_browser(self):
-        """
-        Delete crash dialog and browser cache.
-        """
-        home = os.environ['HOME'].rstrip('/')
-        dotdir = os.path.join(home, '.mozilla/firefox')
-        if not os.path.exists(dotdir):
-            return
-        for profile in os.listdir(dotdir):
-            # Delete cache
-            cachedir = os.path.join(dotdir, profile, 'Cache')
-            if os.path.exists(cachedir):
-                print 'deleting cache', cachedir
-                shutil.rmtree(cachedir)
+    pass
