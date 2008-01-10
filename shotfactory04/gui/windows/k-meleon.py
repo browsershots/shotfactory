0b55387e5b41865149ba62e4431f565b657d01f8
hawk <hawk@1175509c-980b-11dd-9476-71c886a649bc>
2008-01-10 13:17:37 +0000

    - special functions for K-Meleon on Windows
    git-svn-id: http://browsershots.googlecode.com/svn/trunk/shotfactory@2528 1175509c-980b-11dd-9476-71c886a649bc

---
+++ shotfactory04/gui/windows/k-meleon.py
@@ -0,0 +1,74 @@
+# browsershots.org - Test your web design in different browsers
+# Copyright (C) 2007 Johann C. Rocholl <johann@browsershots.org>
+#
+# Browsershots is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Browsershots is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+GUI-specific interface functions for K-Meleon on Microsoft Windows.
+"""
+
+__revision__ = "$Rev$"
+__date__ = "$Date$"
+__author__ = "$Author$"
+
+import os
+import time
+from win32com.shell import shellcon
+from win32com.shell import shell
+from shotfactory04.gui import windows
+
+
+class Gui(windows.Gui):
+    """
+    Special functions for K-Meleon on Windows.
+    """
+
+    def reset_browser(self):
+        """
+        Delete previous session and browser cache.
+        """
+        appdata = shell.SHGetFolderPath(0, shellcon.CSIDL_LOCAL_APPDATA, 0, 0)
+        self.delete_if_exists(os.path.join(
+            appdata, 'K-Meleon', '*', 'Cache'))
+
+    def start_browser(self, config, url, options):
+        """
+        Start browser and load website.
+        """
+        command = config['command'] or r'c:\progra~1\k-meleon\k-meleon.exe'
+        print 'running', command
+        os.spawnl(os.P_DETACH, command, os.path.basename(command), url)
+        print "Sleeping %d seconds while page is loading." % options.wait
+        time.sleep(options.wait)
+
+    def find_scrollable(self):
+        """Find scrollable window."""
+        kmeleon = self.find_window_by_title_suffix(' (K-Meleon)')
+        return self.get_child_window(kmeleon)
+
+
+# Test scrolling from command line
+if __name__ == '__main__':
+    config = {
+        'width': 1024,
+        'bpp': 24,
+        }
+
+    class Options:
+        verbose = 3
+
+    gui = Gui(config, Options())
+    gui.down()
+    time.sleep(1)
+    gui.scroll_bottom()
