2c55f2213de4796ad83aae7d273bc4aed1e74fc4
hawk <hawk@1175509c-980b-11dd-9476-71c886a649bc>
2008-01-12 22:54:59 +0000

    - clear crash dialog, history and cookies too
    git-svn-id: http://browsershots.googlecode.com/svn/trunk/shotfactory@2550 1175509c-980b-11dd-9476-71c886a649bc

--- shotfactory04/gui/windows/flock.py
+++ shotfactory04/gui/windows/flock.py
@@ -41,6 +41,13 @@ class Gui(windows.Gui):
         appdata = shell.SHGetFolderPath(0, shellcon.CSIDL_LOCAL_APPDATA, 0, 0)
         self.delete_if_exists(os.path.join(
             appdata, 'Flock', 'Browsers', 'Profiles', '*', 'Cache'))
+        appdata = shell.SHGetFolderPath(0, shellcon.CSIDL_APPDATA, 0, 0)
+        self.delete_if_exists(os.path.join(
+            appdata, 'Flock', 'Browser', 'Profiles', '*', 'sessionstore.js'))
+        self.delete_if_exists(os.path.join(
+            appdata, 'Flock', 'Browser', 'Profiles', '*', 'history.dat'))
+        self.delete_if_exists(os.path.join(
+            appdata, 'Flock', 'Browser', 'Profiles', '*', 'cookies.txt'))
 
     def start_browser(self, config, url, options):
         """
